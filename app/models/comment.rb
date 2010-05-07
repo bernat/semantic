fceac2b73fd7d1db17cd65a9665fbdf1eacd5f2a
Bernat Farrero <dberni@gmail.com>
2010-05-07 17:59:56 +0200

    Antispam super-cutre

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :episode
+  attr_accessor :antispam
   
   validates_presence_of :name, :on => :create, :message => "És obligatori introduir un nom"
   validates_presence_of :email, :on => :create, :message => "És obligatori introduir un email"
   validates_presence_of :content, :on => :create, :message => "No has escrit res!"
+  
+  def before_create
+    @antispam == "26"
+  end
   
   # def request=(request)
   #    self.user_ip    = request.remote_ip
