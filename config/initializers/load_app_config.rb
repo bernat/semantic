4947c8fffe65e3fff96dd619b6f4e1dec1fc35d0
Jordi Romero <jordi@jrom.net>
2010-05-07 13:00:13 +0200

    Ara farem servir el fitxer app.yml només amb la configuració en development, les dades en producció les treurem de variables d'entorn (especial Heroku) No és la millor solució, però per heroku funciona.

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,3 +1,6 @@
-# APP_CONFIG = YAML.load_file("#{Rails.root}/config/app.yml")[Rails.env]
-APP_CONFIG = []
-
+if Rails.env.production?
+  APP_CONFIG['username'] = ENV['username']
+  APP_CONFIG['password'] = ENV['password']
+else
+  APP_CONFIG = YAML.load_file("#{Rails.root}/config/app.yml")
+end
