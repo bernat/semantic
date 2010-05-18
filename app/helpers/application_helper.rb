3bc4591542f07d48c6a816fdeec020492471ad7a
Jordi Romero <jordi@jrom.net>
2010-05-18 18:07:12 +0200

    Fixing bug pretty_date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,17 +24,15 @@ module ApplicationHelper
          url << "?#{opts.join('&')}" unless opts.empty?
        end
     end
-    
-  def pretty_date(date)
-    return '' if date.nil?
-    return 'Pròximament' if date > Date.today 
-    date.strftime('%d/%m/%Y')
-    dataparsed = Time.parse(date.strftime('%d/%m/%Y')).utc #Fix problem UTC vs Local Zone
-    if dataparsed > 2.day.ago
-          "Fa #{time_ago_in_words date}"
-        else
-          "Emès el #{date.strftime('%d/%m/%Y')}"
-        end
+
+    def pretty_date(date)
+      return '' if date.nil?
+      return 'Pròximament' if date > Date.today
+      if date.to_time.utc > 2.day.ago
+        "Fa #{time_ago_in_words date}"
+      else
+        "Emès el #{date.strftime('%d/%m/%Y')}"
+      end
     end
-  
+
 end
