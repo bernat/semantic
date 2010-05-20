568c39ada77ef6a9034d0d2fa69efb0b9399875a
bernat <dberni@gmail.com>
2010-05-20 20:02:26 +0200

    small fix based on masu requests

--- public/javascripts/efectes.js
+++ public/javascripts/efectes.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 });
 
 $.easing.bouncy = function (x, t, b, c, d) { 
-    var s = 1.70158; 
+    var s = 5; 
     if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b; 
     return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b; 
 }
@@ -23,12 +23,12 @@ $.tools.tooltip.addEffect("bouncy",
  
     // opening animation 
     function(done) { 
-        this.getTip().animate({top: '+=15'}, 500, 'bouncy', done).show(); 
+        this.getTip().animate({top: '+=5'}, 500, 'bouncy', done).show(); 
     }, 
  
     // closing animation 
     function(done) { 
-        this.getTip().animate({top: '-=15'}, 500, 'bouncy', function()  { 
+        this.getTip().animate({top: '-=5'}, 500, 'bouncy', function()  { 
             $(this).hide(); 
             done.call(); 
         }); 
