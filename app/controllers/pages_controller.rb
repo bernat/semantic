71c8166e1380979230182043e421a959031be3da
bernat <dberni@gmail.com>
2010-05-23 19:39:56 +0200

    Tag name validation and participant refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,21 @@ class PagesController < ApplicationController
   end
   
   def bernat
+    @participant = 'bernat'
+    @participant_email = 'bernat@itnig.net'
+    render 'participant'
   end
   
   def jordi
+    @participant = 'jordi'
+    @participant_email = 'jordi@jrom.net'
+    render 'participant'
   end
   
   def masumi
+    @participant = 'masumi'
+    @participant_email = 'mutsuda@gmail.com'
+    render 'participant'
   end
 
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,9 @@
 class Tag < ActiveRecord::Base
   has_many :taggings, :dependent => :destroy
   has_many :episodes, :through => :taggings
-  
+
+  validates :name, :presence => true
+
   def self.with_names(names)
     names.map do |name|
       Tag.find_or_create_by_name(name)
