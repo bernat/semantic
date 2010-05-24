bb96dde4d101ac163d58b36b715c0d2fed930dfd
Jordi Romero <jordi@jrom.net>
2010-05-24 13:29:50 +0200

    Desfer hack cutre heroku, load app.yml

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,7 +1,2 @@
-if Rails.env.production?
-  APP_CONFIG = {}
-  APP_CONFIG['username'] = ENV['username']
-  APP_CONFIG['password'] = ENV['password']
-else
-  APP_CONFIG = YAML.load_file("#{Rails.root}/config/app.yml")
-end
+
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/app.yml")
