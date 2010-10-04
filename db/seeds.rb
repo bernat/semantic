2cb9f393c4b6483ec0ab30be97d2b0bdb525d3bf
Jordi Romero <jordi@jrom.net>
2010-10-04 22:22:40 +0200

    Better seed data. Still Screencasts are missing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,18 @@
-puts "######################## SEED DATA ########################"
+emails = %w(bernat@itnig.net jordi@jrom.net mutsuda@gmail.com david@loudthinking.com juanico@ramires.cat)
+tags = %w(Rails Web Apple Database Javascript Microsoft Google)
 
-puts "Adding 20 Sample Episodes..."
-50.times do |i|
-  Episode.create! :title => "Episodi #{i}",
-                  :permalink => "episode-#{i}",
-                  :description => "Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse leo purus, rutrum in ullamcorper eget, auctor a lacus. Quisque a facilisis est. Sed in blandit diam. Nam gravida, felis ac elementum elementum, eros metus consectetur ipsum, at eleifend quam erat nec ante. Aenean sit amet nulla eros, sit amet vehicula purus. Vivamus volutpat arcu a arcu sagittis aliquam. Donec hendrerit semper mauris, eget imperdiet enim posuere nec. Duis aliquet eleifend vehicula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam purus justo, dapibus vel tempus quis, varius eu augue. Praesent ligula tortor, pellentesque nec vestibulum vitae, malesuada in lacus. Vestibulum fermentum enim quis magna posuere nec pretium arcu porttitor. Cras eu leo non sapien dapibus dapibus id vel nunc. Morbi eu placerat nulla.",
-                  :aired_on => Date.today,
-                  :notes => "Notes",
-                  :number => i
+puts "Adding 15 Sample Episodes..."
+15.times do |i|
+  e = Episode.create! :title => "Episodi #{i}",
+                      :permalink => "episode-#{i}",
+                      :description => "Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse leo purus, rutrum in ullamcorper eget, auctor a lacus. Quisque a facilisis est. Sed in blandit diam. Nam gravida, felis ac elementum elementum, eros metus consectetur ipsum, at eleifend quam erat nec ante. Aenean sit amet nulla eros, sit amet vehicula purus. Vivamus volutpat arcu a arcu sagittis aliquam. Donec hendrerit semper mauris, eget imperdiet enim posuere nec. Duis aliquet eleifend vehicula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam purus justo, dapibus vel tempus quis, varius eu augue. Praesent ligula tortor, pellentesque nec vestibulum vitae, malesuada in lacus. Vestibulum fermentum enim quis magna posuere nec pretium arcu porttitor. Cras eu leo non sapien dapibus dapibus id vel nunc. Morbi eu placerat nulla.",
+                      :aired_on => Date.today - (15 - i).days,
+                      :notes => "Notes",
+                      :tag_names => tags.sample(rand(5)).join(' '),
+                      :number => i
+
+  rand(5).times do |ci|
+    person = emails.sample
+    e.comments.create! :name => person.split('@').first.humanize, :email => person, :content => "Interesting..."
+  end
 end
-
-puts "######################## END SEED DATA ########################"
